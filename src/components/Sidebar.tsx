ca3fc85d97ac3d9f80b3657cbc174453ad753ff3
Igor Almeida <igor.almeida@madeinweb.app>
2024-01-26 20:05:55 -0300

    [feat] sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import {useState, useEffect} from "react";
+import ProfileItem from "./ProfileItem";
+import SongItem from "./SongItem";
+import { SongData } from "../models/song.interface";
+import {FiPlus } from "react-icons/fi";
+import api from "../services/api";
+
+export default function Sidebar() {
+    const [songs, setSongs] = useState<SongData[]>([])
+
+    useEffect(() => {
+        api.getSongs().then(res => setSongs(res))
+    },[])
+
+
+    return (
+        <div className="border-r-2 h-screen container w-72">
+            <ProfileItem />
+            {/* Separator */}
+            <div className="h-min w-full bg-slate-200"></div>
+            <div className="flex justify-between items-center px-4 pt-4">
+                <p className="text-slate-500 text-xs">MY SONGS</p>
+                <FiPlus color="rgb(100 116 139)"/>
+            </div>
+            <div>
+                {songs != undefined ? songs!.map((song) => <SongItem key={song.id} title={song.title}/> ) : null}
+            </div>
+        </div>
+    )
+}
